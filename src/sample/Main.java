464322a45ce08e4823dc4e29ea0e30ee1db336a0
Franek Wołoch <franciszek.woloch@gmail.com>
2019-01-30 00:05:38 +0100

    changed window title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        final String appName="PESELchecker";
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle(appName);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
