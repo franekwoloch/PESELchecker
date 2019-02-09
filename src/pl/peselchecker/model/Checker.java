6d9fdffab25585a6978508a49c4becf02d4756e2
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-09 11:34:19 +0100

    added lombok library and start created InfoPack class - second commit

--- src/pl/peselchecker/model/Checker.java
+++ src/pl/peselchecker/model/Checker.java
@@ -1,4 +1,22 @@
 package pl.peselchecker.model;
 
-public class Checker {
+@FunctionalInterface
+public interface Checker {
+
+    //p - pesel number
+    boolean check(int [] p){
+        int sum;
+        double remainder;
+        boolean result;
+        sum=p[0]+p[1]*3+p[2]*7+p[3]*9+p[4]+p[5]*3+p[6]*7+p[7]*9+p[8]+p[9]*3+p[10];
+        remainder=int%10;
+        if (remainder==0){
+            result=true;
+        } if else {
+            result== false;
+        }
+
+        return result;
+    }
+
 }
--- src/pl/peselchecker/model/InfoPack.java
+++ src/pl/peselchecker/model/InfoPack.java
@@ -1,4 +1,10 @@
 package pl.peselchecker.model;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class InfoPack {
+    private LocalDate birthDate;
 }
