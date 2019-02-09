adaa064f5aa3fafb854bb32278696b86b09a4291
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-09 11:52:48 +0100

    added sex decoding in Decoder Interface

--- src/pl/peselchecker/model/Decoder.java
+++ src/pl/peselchecker/model/Decoder.java
@@ -1,4 +1,21 @@
 package pl.peselchecker.model;
 
-public class Decoder {
+import java.time.LocalDate;
+
+@FunctionalInterface
+public interface Decoder {
+    //p-pesel number
+
+    InfoPack decode(int[] p){
+        private LocalDate birthDate;
+        private Sex sex;
+        private int sexIndex=p[9]%2;
+
+        if (sexIndex==0){
+            sex=Sex.F;
+        } else if {
+            sex=Sex.M;
+        }
+
+    }
 }
