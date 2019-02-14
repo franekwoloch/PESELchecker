0df6fdaf48179e11c0741a9d8212c237d619e1a0
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-14 22:51:40 +0100

    File reader updated

--- src/pl/peselchecker/model/FileReader.java
+++ src/pl/peselchecker/model/FileReader.java
@@ -1,9 +1,12 @@
 package pl.peselchecker.model;
 
+import jdk.nashorn.internal.parser.Scanner;
+
 @FunctionalInterface
 public interface FileReader {
 
-    DataBase readFile(){
+    DataBase readFile(String path, String fileName){
+        Scanner scanner=new Scanner(fileName);
         
     }
 }
