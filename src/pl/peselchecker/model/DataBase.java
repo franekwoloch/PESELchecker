bbdae56ccff7610568bb3997607f1aea8fb5e005
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-14 21:09:29 +0100

    developed DataBase class

--- src/pl/peselchecker/model/DataBase.java
+++ src/pl/peselchecker/model/DataBase.java
@@ -1,4 +1,13 @@
 package pl.peselchecker.model;
 
 public class DataBase {
+
+    Integer [] pesels = new Integer[1000];
+    String [] descriptions = new String[1000];
+
+    Object [][] DataBase = new Object[2][];
+    DataBase[0]=pesels;
+    DataBase[1]=descriptions;
+
+
 }
