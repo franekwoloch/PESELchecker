0213cc4db7329b6cc0397396bd76e5bcc0a4ba81
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-16 00:42:40 +0100

    finished FileReaderInterface

---
+++ src/pl/peselchecker/model/FileReaderInterface.java
@@ -0,0 +1,93 @@
+package pl.peselchecker.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface FileReaderInterface {
+
+    DataBase readFile(String path, String fileName){
+
+        DataBase newDataBase=new DataBase();
+
+        //Creating temporary DataBase
+        String [] tempRecords=new String[1000];
+        FileReader fileReader = null;
+        BufferedReader reader = null;
+        String fileName= "file.txt";
+        int tempRecordsLength=0;
+
+
+        //reading file to temporary array
+        try {
+            fileReader = new java.io.FileReader(fileName);
+            reader = new BufferedReader(fileReader);
+            String nextLine = null;
+            int lines = 0;
+            while ((nextLine = reader.readLine()) != null) {
+                tempRecords[lines]=nextLine;
+                tempRecordsLength=lines+1;
+                lines++;
+            }
+            System.out.println("Wczytano " + lines+" rekordow");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String tempRecord;
+        StringBuilder tempDescription=null;
+        int [] tempPesel=new int [9];
+        int tempCharsIndex=0;
+
+        for (int i=0; i<tempRecordsLength; i++) {
+            //record analysis
+            tempRecord = tempRecordsLength[i];
+            char[] tempCharsArray;
+            tempCharsArray = tempRecord.toCharArray();
+            int tempCharsLength = tempCharsArray.length; //length of record
+            //reading first nine chars
+            for (int j = 0; j < 9; j++) {
+                tempCharsIndex=j;
+                char testChar = tempCharsArray[j];
+                if (Character.isDigit(testChar)) {
+                    tempPesel[j] = Character.getNumericValue(testChar);
+                    if (Character.isSpaceChar(tempCharsArray[j + 1])) {
+                        tempCharsIndex = j + 1;
+                        break;
+                    }
+                }
+            }
+            //check length of Pesel
+            int difference;
+            if (tempCharsIndex<9) {
+                difference=9-tempCharsIndex;
+                for (int k=8; k>=difference;k--){
+                    tempPesel[k]=tempPesel[k-difference]
+            }
+            for (int l=0;l<difference;l++){
+                    tempPesel[l]=0;
+            }
+            Pesel peselToSave = new Pesel(tempPesel);
+
+                //save pesel to DataBase
+            newDataBase.pesels[i]=peselToSave;
+            }
+            //description analysis
+            for (int m=tempCharsIndex+1;m<tempCharsLength;m++){
+                tempDescription=tempDescription.append(tempCharsArray[m]);
+            }
+
+            newDataBase.descriptions[i]=tempDescription.toString();
+
+        }
+        return newDataBase;
+    }
+}
