2b8fe95886203c5bc4d75d9dfee4071bed6781b1
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-16 15:17:58 +0100

    DataBase class review

--- src/pl/peselchecker/model/DataBase.java
+++ src/pl/peselchecker/model/DataBase.java
@@ -5,9 +5,6 @@ public class DataBase {
     Pesel[] pesels = new Pesel[1000];
     String [] descriptions = new String[1000];
 
-    Object [][] DataBase = new Object[2][];
-    DataBase[0]=pesels;
-    DataBase[1]=descriptions;
-
+    Object [][] DataBase = {pesels, descriptions};
 
 }
