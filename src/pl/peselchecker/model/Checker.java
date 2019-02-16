ce2ceb4334798d579313c2a62b7562c841690f90
Franek Wołoch <franciszek.woloch@gmail.com>
2019-02-16 15:02:23 +0100

    Checker class review

--- src/pl/peselchecker/model/Checker.java
+++ src/pl/peselchecker/model/Checker.java
@@ -1,7 +1,6 @@
 package pl.peselchecker.model;
 
-@FunctionalInterface
-public interface Checker {
+public class Checker {
 
     //p - pesel number
     boolean check(int [] p){
@@ -9,11 +8,11 @@ public interface Checker {
         double remainder;
         boolean result;
         sum=p[0]+p[1]*3+p[2]*7+p[3]*9+p[4]+p[5]*3+p[6]*7+p[7]*9+p[8]+p[9]*3+p[10];
-        remainder=int%10;
+        remainder=sum%10;
         if (remainder==0){
             result=true;
-        } if else {
-            result== false;
+        } else {
+            result= false;
         }
 
         return result;
